8bfac5670d4fab7604f1eb9cbbfafa2ccdb1bcc0
laifengting <laifengting@foxmail.com>
2021-02-22 20:49:51 +0800

    JVM-下篇:性能监控与调优

--- chapter03/src/com/atguigu/jprofiler/JProfilerTest.java
+++ chapter03/src/com/atguigu/jprofiler/JProfilerTest.java
@@ -1,7 +1,6 @@
 package com.atguigu.jprofiler;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,23 +9,24 @@ import java.util.concurrent.TimeUnit;
  * @create 12:19
  */
 public class JProfilerTest {
-    public static void main(String[] args) {
-        while (true){
-            ArrayList list = new ArrayList();
-            for (int i = 0; i < 500; i++) {
-                Data data = new Data();
-                list.add(data);
-            }
-            try {
-                TimeUnit.MILLISECONDS.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	public static void main(String[] args) {
+		while (true) {
+			ArrayList list = new ArrayList();
+			for (int i = 0; i < 500; i++) {
+				Data data = new Data();
+				list.add(data);
+			}
+			try {
+				TimeUnit.MILLISECONDS.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
-class Data{
-    private int size = 10;
-    private byte[] buffer = new byte[1024 * 1024];//1mb
-    private String info = "hello,atguigu";
+
+class Data {
+	private int size = 10;
+	private byte[] buffer = new byte[1024 * 1024];//1mb
+	private String info = "hello,atguigu";
 }
--- chapter03/src/com/atguigu/jprofiler/MemoryLeak.java
+++ chapter03/src/com/atguigu/jprofiler/MemoryLeak.java
@@ -1,8 +1,6 @@
 package com.atguigu.jprofiler;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,28 +8,28 @@ import java.util.concurrent.TimeUnit;
  * @create 12:57
  */
 public class MemoryLeak {
-
-    public static void main(String[] args) {
-        while (true) {
-            ArrayList beanList = new ArrayList();
-            for (int i = 0; i < 500; i++) {
-                Bean data = new Bean();
-                data.list.add(new byte[1024 * 10]);//10kb
-                beanList.add(data);
-            }
-            try {
-                TimeUnit.MILLISECONDS.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
+	
+	public static void main(String[] args) {
+		while (true) {
+			ArrayList beanList = new ArrayList();
+			for (int i = 0; i < 500; i++) {
+				Bean data = new Bean();
+				data.list.add(new byte[1024 * 10]);//10kb
+				beanList.add(data);
+			}
+			try {
+				TimeUnit.MILLISECONDS.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
 
 class Bean {
-    int size = 10;
-    String info = "hello,atguigu";
-     ArrayList list = new ArrayList();
-//    static ArrayList list = new ArrayList();
+	int size = 10;
+	String info = "hello,atguigu";
+	ArrayList list = new ArrayList();
+	// static ArrayList list = new ArrayList();
 }
